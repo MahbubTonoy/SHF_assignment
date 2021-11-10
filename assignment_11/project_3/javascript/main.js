75e517925d8161dd5fdee6c9027bbcda44ef2a98
MahbubTonoy <tnymrbadboy@gmail.com>
2021-11-11 01:05:54 +0600

    build: assignment_11 project_3 complete

---
+++ assignment_11/project_3/javascript/main.js
@@ -0,0 +1,38 @@
+var friends = [
+  "Cartoon Fahid",
+  "Masud Rana",
+  "Lucy Reza",
+  "Maria Dewan",
+  "Md Abdul Alim",
+  "Md Serazul Islam",
+  "RA Shawon",
+  "A.N. Pronoy",
+  "Anwar Hossain",
+  "MD Mohsinul Haque",
+  "Saiimum",
+  "Hridoy Ahad",
+  "Jannat Chaiti",
+  "Mahdee Al Amin",
+  "Mbs Mahedi Hasan",
+  "Shawon Ahmed Rubel",
+  "Sakib Raz"
+];
+
+function greaterNameFinder(friendsArray) {
+ var targetIndex = 0;
+
+ for(var i = 0; i < friendsArray.length; i++) {
+
+  if(friendsArray[i].length > friendsArray[targetIndex].length) {
+   targetIndex = i;
+  }
+
+ }
+
+ return friendsArray[targetIndex];
+}
+
+
+var greaterName = greaterNameFinder(friends);
+
+document.write("<h2>Greater Name of My Friendlist is: <i>"+greaterName+"</i></h2>");
