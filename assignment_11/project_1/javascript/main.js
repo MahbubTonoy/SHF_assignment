cda19147c8f45edbae6b0f17fd91dc42fd940d5b
MahbubTonoy <tnymrbadboy@gmail.com>
2021-11-13 23:42:47 +0600

    fix: some changes in project_1

--- assignment_11/project_1/javascript/main.js
+++ assignment_11/project_1/javascript/main.js
@@ -1,8 +1,15 @@
 function totalPrice(watch, laptop, phone) {
- var total = watch+laptop+phone;
- return total;
+  var total = watch + laptop + phone;
+  return total;
 }
 
+let watch = 4999;
+let laptop = 70500;
+let phone = 20000;
 
-var total_price = totalPrice(4999, 70500, 20000); //sequence: watch, laptop, phone
-document.write("<h2>Total Price: "+total_price+ " BDT</h2>");
+var total_price = totalPrice(watch, laptop, phone); //sequence: watch, laptop, phone
+
+document.write("<h2>Watch &Tab;= " + watch + "BDT</h2>");
+document.write("<h2>Laptop = " + laptop + "BDT</h2>");
+document.write("<h2>Phone = " + phone + "BDT</h2><hr/>");
+document.write("<h2>Total Price = " + total_price + " BDT</h2>");
